987da9971b5becbeb58321c8db53de49d51b6ce4
Vajra-Phil <71310997+Vajra-Phil@users.noreply.github.com>
2022-03-22 16:15:31 +0100

    codice sbagliato

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 "use strict";
 
-let list = document.getElementsByClassName("list-group prodotti");
+let list = document.querySelectorAll("list-group-item prodotto");
+
+console.log(list[0]);
+list[0].className = "list-group-item prodotto";
+
+function click(){
+    list[0].className = "list-group-item prodotto";
+};
+
 for(let item in list)
 {
     item.addEventListener("click", click);
-    function click(){
-        item.className = "list-group-item prodotto active";
-    };
 }
+list[0].addEventListener("click", click);
+
+
+
 
 
 let listActive = document.getElementsByClassName("list-group-item prodotto active");
 
 for(let item in listActive){
     item.addEventListener("click", clickDisable);
-    function clickDisable(){
-        item.className = "list-group-item prodotto";
-    };
 }
+
+function clickDisable(){
+    document.getElementById("1").className = "list-group-item prodotto";
+};
 
